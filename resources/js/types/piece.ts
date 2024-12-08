0ac2abfa6d4c247687c84891289aa4deb9672282
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-08 23:35:38 +0200

    [front-setup] Add basic types for piece

---
+++ resources/js/types/piece.ts
@@ -0,0 +1,20 @@
+export type PieceCode = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+export type PieceRotation = 0 | 1 | 2 | 3; //* [0deg, 90deg, 180deg, 270deg]
+
+export type MovePayload = {
+    code: PieceCode; //! [0-20]
+    origin_position: Vector2; //! [0-19]
+    rotation: PieceRotation;
+    flip: boolean;
+}
+
+export type PieceCenterData = {
+    center_offset: Vector2;
+    origin_center_distance: Vector2;
+    code: PieceCode;
+}
+
+export type Vector2 = {
+    x: number;
+    y: number;
+}
