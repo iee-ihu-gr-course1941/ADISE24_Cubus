a3d0b88d648649e5b60c92c08ffb9cc455c27410
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-08 23:52:50 +0200

    [front_setup] add a basic scene

--- resources/js/Game/Experience.tsx
+++ resources/js/Game/Experience.tsx
@@ -1,7 +1,22 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 export const Experience = () => {
     return (
-        <div>
-            <h1>Experience</h1>
-        </div>
+        <Canvas camera={{
+            fov: 60,
+            near: 0.1,
+            far: 200,
+            position: [-1.5, 2, 10],
+          }}>
+            <OrbitControls/>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            <color attach={'background'} args={['#171717']} />
+            <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color="hotpink" />
+            </mesh>
+        </Canvas>
     );
 }
