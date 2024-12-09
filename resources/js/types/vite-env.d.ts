2c8d57780ece46bf51bd21108b80efab91bf0ab8
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-09 15:37:08 +0200

    [front_setup] add threejs types

--- resources/js/types/vite-env.d.ts
+++ resources/js/types/vite-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="vite/client" />
+
+import { ShaderMaterialProps } from "@react-three/fiber"
+
+declare global {
+  module "*.glsl"{
+    const value: string
+    export default value
+  }
+  interface ThreeElements {
+    customMaterial: ShaderMaterialProps;
+    // * New extend types go here !
+  }
+  namespace JSX {
+      // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+      interface IntrinsicElements extends ThreeElements {
+      }
+  }
+}
