b6d7231164cb97773fbda038de950ef076a37f39
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-10 15:00:13 +0200

    Fix duplicate renders

--- resources/js/Game/Experience.tsx
+++ resources/js/Game/Experience.tsx
@@ -3,20 +3,23 @@ import { Canvas } from "@react-three/fiber";
 import { Board } from "./Board/Board";
 import { Lights } from "./Ligths";
 import { Perf } from "r3f-perf"
+import { memo } from "react";
 
-export const Experience = () => {
-    return (
-        <Canvas camera={{
-            fov: 60,
-            near: 0.1,
-            far: 200,
-            position: [-1.5, 2, 10],
-          }}>
-            <OrbitControls makeDefault/>
-            <color attach={'background'} args={['#535353']} />
-            <Lights/>
-            <Board/>
-            <Perf position="top-left"/>
-        </Canvas>
-    );
-}
+export const Experience = memo(
+    () => {
+        return (
+            <Canvas camera={{
+                fov: 60,
+                near: 0.1,
+                far: 200,
+                position: [3.5, 5, 10],
+              }}>
+                <OrbitControls makeDefault/>
+                <color attach={'background'} args={['#535353']} />
+                <Lights/>
+                <Board/>
+                <Perf position="top-left"/>
+            </Canvas>
+        );
+    }
+);
