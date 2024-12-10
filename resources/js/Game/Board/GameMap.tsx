54fcf8f0cf0a7a423b50f21a78d3c83fc7868bf2
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-10 17:57:52 +0200

    [Pieces] Add GameMap

---
+++ resources/js/Game/Board/GameMap.tsx
@@ -0,0 +1,17 @@
+import { Lights } from "../Ligths";
+import { Board } from "./Board";
+import Hand from "./Hand";
+
+const GameMap = () => {
+
+    return (
+        <>
+            <Lights/>
+            <Board/>
+            <Hand/>
+        </>
+    )
+
+}
+
+export default GameMap;
