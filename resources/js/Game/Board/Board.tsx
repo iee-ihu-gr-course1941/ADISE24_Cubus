75e62ddb25f400643c574b70c11499ff636b0509
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-10 15:13:49 +0200

    add correct piece props

--- resources/js/Game/Board/Board.tsx
+++ resources/js/Game/Board/Board.tsx
@@ -11,10 +11,14 @@ export const Board = () => {
         <>
             <Grid args={[gridSize, gridSize, 1]} sectionSize={blockSize} sectionColor={0x00ff00} />
             {
-                new Array(20).fill(0).map((_, index) => {
-                    const position = new THREE.Vector3((Math.random() - 0.5) * 10, 0, (Math.random() -0.5) * 10);
+                new Array(21).fill(0).map((_, index) => {
+                    const position = new THREE.Vector3((Math.random() - 0.5) * 15, 0, (Math.random() -0.5) * 15);
+                    position.x = Math.round(position.x * blockSize) / blockSize;
+                    position.z = Math.round(position.z * blockSize) / blockSize;
+                    const rotation = Math.floor(Math.random() * 4);
+                    const flip = Math.random() > 0.5;
                     return (
-                        index === 8 && <Piece key={index} pieceCode={index as PieceCode} position={[0, 0.25, 0]}/>
+                      index > 15 && <Piece key={index} code={index as PieceCode} origin_position={{x: position.x, y: position.z}} rotation={rotation} flip={flip}/>
                     )
                 })
             }
