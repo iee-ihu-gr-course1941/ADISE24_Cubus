075c5a5da48ce23d0128895e107e6728d8b881d0
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-11 16:32:46 +0200

    [Pieces] Add board pieces storage

--- resources/js/Store/board_state.ts
+++ resources/js/Store/board_state.ts
@@ -4,15 +4,24 @@ import * as THREE from "three";
 
 type State = {
     boardRef: THREE.Mesh | null
+    boardPieces: THREE.Group[]
 }
 
 type Actions = {
     setBoardRef: (boardRef: THREE.Mesh | null) => void
+    addBoardPiece: (piece: THREE.Group) => void;
 }
 
-export const useBoardState = create<State & Actions>()((set, get, state) => ({
+export type BoardState = State & Actions
+
+export const useBoardState = create<BoardState>()((set, get, state) => ({
     boardRef: null,
+    boardPieces: [],
     setBoardRef: (boardRef) => {
         set({boardRef})
-    }
+    },
+    addBoardPiece: (piece) => set((prev) => {
+        if(prev.boardPieces.find(p => p.uuid === piece.uuid)) return prev
+        return {boardPieces: [...prev.boardPieces,piece]}
+    }),
 }));
