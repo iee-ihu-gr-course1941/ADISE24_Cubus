0cf7b13f61eaa9250afd566627b1f652cc0645c3
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-11 19:11:17 +0200

    [Game-Mechanics] Add Interface on page

--- resources/js/Game/Experience.tsx
+++ resources/js/Game/Experience.tsx
@@ -5,21 +5,25 @@ import { Lights } from "./Ligths";
 import { Perf } from "r3f-perf"
 import { memo } from "react";
 import GameMap from "./Board/GameMap";
+import { Interface } from "./Interface";
 
 export const Experience = memo(
     () => {
         return (
-            <Canvas camera={{
-                fov: 60,
-                near: 0.1,
-                far: 200,
-                position: [14.5, 5, 15],
-              }}>
-                <Perf position="top-left"/>
-                <OrbitControls makeDefault/>
-                <color attach={'background'} args={['#535353']} />
-                <GameMap/>
-            </Canvas>
+            <>
+                <Canvas camera={{
+                    fov: 60,
+                    near: 0.1,
+                    far: 200,
+                    position: [14.5, 5, 15],
+                }}>
+                    <Perf position="top-left"/>
+                    <OrbitControls makeDefault/>
+                    <color attach={'background'} args={['#535353']} />
+                    <GameMap/>
+                </Canvas>
+                <Interface/>
+            </>
         );
     }
 );
