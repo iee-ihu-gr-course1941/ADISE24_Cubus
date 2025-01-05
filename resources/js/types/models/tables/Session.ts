d707464a877b38bd05694e3fc291453ea4c3883d
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-05 16:35:00 +0200

    adjust session type

--- resources/js/types/models/tables/Session.ts
+++ resources/js/types/models/tables/Session.ts
@@ -1,3 +1,4 @@
+import { SessionState, ValorizedVector2 } from '@/types/game';
 import {Id, ForeignId, Timestamp, Timestamps} from '../index';
 import { User } from './User';
 
@@ -5,11 +6,10 @@ export type Game_session = {
     id: Id;
     timestamps: Timestamp;
     current_round: number; //* [Default value]: 0
-    board_state?: string;
-    session_state?: string;
+    board_state?: Array<ValorizedVector2>;
+    session_state?: SessionState;
     player_blue?: User;
     player_red?: User;
     player_green?: User;
     player_yellow?: User;
 }
-
