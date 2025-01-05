651dfb4e6b5d05d7d0eaa4d4b24a1631d9c4ef20
Dale Bezolli <dalebezolli@gmail.com>
2025-01-05 13:02:43 +0200

    [feat-contract] Lock game channels

--- routes/channels.php
+++ routes/channels.php
@@ -3,7 +3,12 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('game.{id}', function (User $user, string $id) {
-    error_log('WTF:::: ' . $id);
+Broadcast::channel('game.{id}', function (User $user, int $id) {
+    $user_current_session_id = $user->getCurrentSession()['id'];
+
+    error_log('Attempting to connect user in ' . $id . ' that\'s in session ' . $user_current_session_id);
+
+    if($user_current_session_id !== $id) return false;
+    error_log('Connected user ' . $user['name'] . '(' . $user['id'] . ') successfully');
     return true;
 });
