0fe6992b3bd6bd10145f2fe3fcef33a0652c7bcc
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-06 21:13:22 +0200

    adjust camera positions based on ui state

--- resources/js/Game/Board/BoardControls.tsx
+++ resources/js/Game/Board/BoardControls.tsx
@@ -1,21 +1,29 @@
-import { DESTINATION_CAMERA_LOOK_AT } from "@/Constants/camera";
-import { useBoardState } from "@/Store/board_state";
-import { OrbitControls } from "@react-three/drei"
-import * as THREE from "three";
+import {DESTINATION_CAMERA_LOOK_AT} from '@/Constants/camera';
+import {useBoardState} from '@/Store/board_state';
+import {OrbitControls} from '@react-three/drei';
+import * as THREE from 'three';
 
 export const BoardControls = () => {
-    const state = useBoardState(state => state.gameState.state)
+    const ui_state = useBoardState(state => state.gameState.ui_state);
     return (
         <>
             <OrbitControls
-                enabled={state !== 'Ready' && state !== 'Finished' && state !== 'Starting'}
+                enabled={
+                    ui_state === 'OpponentTurn' ||
+                    ui_state === 'OwnTurnLocked' ||
+                    ui_state === 'OwnTurnPlaying'
+                }
                 target={DESTINATION_CAMERA_LOOK_AT}
-
-                minPolarAngle={Math.PI * 0.025} maxPolarAngle={Math.PI * 0.35}
-                maxAzimuthAngle={Math.PI * 0.05} minAzimuthAngle={Math.PI * -0.05}
-
-                makeDefault enableDamping
+                minPolarAngle={Math.PI * 0.025}
+                maxPolarAngle={Math.PI * 0.35}
+                maxAzimuthAngle={Math.PI * 0.05}
+                minAzimuthAngle={Math.PI * -0.05}
+                minDistance={4}
+                maxDistance={16}
+                enableZoom={true}
+                makeDefault
+                enableDamping
             />
         </>
-    )
-}
+    );
+};
--- resources/js/Game/Experience.tsx
+++ resources/js/Game/Experience.tsx
@@ -1,38 +1,81 @@
-import { OrbitControls } from "@react-three/drei";
-import { Camera, Canvas, Object3DNode, useThree } from "@react-three/fiber";
-import { Board } from "./Board/Board";
-import { Lights } from "./Ligths";
-import { Perf } from "r3f-perf"
-import { memo, useEffect, useMemo, useRef } from "react";
-import GameMap from "./Board/GameMap";
-import { Interface } from "./Interface";
-import { BoardControls } from "./Board/BoardControls";
-import * as THREE from "three";
-import { ORIGIN_CAMERA_LOOK_AT, ORIGIN_CAMERA_POSITION } from "@/Constants/camera";
+import {OrbitControls} from '@react-three/drei';
+import {Camera, Canvas, Object3DNode, useThree} from '@react-three/fiber';
+import {Board} from './Board/Board';
+import {Lights} from './Ligths';
+import {Perf} from 'r3f-perf';
+import {memo, useEffect, useMemo, useRef} from 'react';
+import GameMap from './Board/GameMap';
+import {Interface} from './Interface';
+import {BoardControls} from './Board/BoardControls';
+import * as THREE from 'three';
+import {
+    DESTINATION_CAMERA_LOOK_AT,
+    DESTINATION_CAMERA_POSITION,
+    FINISHED_CAMERA_LOOK_AT,
+    FINISHED_CAMERA_POSITION,
+    ORIGIN_CAMERA_LOOK_AT,
+    ORIGIN_CAMERA_POSITION,
+} from '@/Constants/camera';
+import {useBoardState} from '@/Store/board_state';
 
 const INITIAL_CAMERA_PROPS = {
     fov: 85,
     near: 0.1,
     far: 200,
-}
+};
 
-export const Experience = memo(
-    () => {
-        return (
-            <>
-                <Canvas
+export const Experience = memo(() => {
+    const ui_state = useBoardState(state => state.gameState.ui_state);
+    return (
+        <>
+            <Canvas
                 onCreated={({camera}) => {
-                    camera.position.set(ORIGIN_CAMERA_POSITION.x, ORIGIN_CAMERA_POSITION.y, ORIGIN_CAMERA_POSITION.z);
-                    camera.lookAt(ORIGIN_CAMERA_LOOK_AT.x, ORIGIN_CAMERA_LOOK_AT.y, ORIGIN_CAMERA_LOOK_AT.z);
+                    if (ui_state === 'Ready') {
+                        camera.position.set(
+                            ORIGIN_CAMERA_POSITION.x,
+                            ORIGIN_CAMERA_POSITION.y,
+                            ORIGIN_CAMERA_POSITION.z,
+                        );
+                        camera.lookAt(
+                            ORIGIN_CAMERA_LOOK_AT.x,
+                            ORIGIN_CAMERA_LOOK_AT.y,
+                            ORIGIN_CAMERA_LOOK_AT.z,
+                        );
+                    } else if (
+                        ui_state === 'OpponentTurn' ||
+                        ui_state === 'OwnTurnPlaying' ||
+                        ui_state === 'OwnTurnLocked'
+                    ) {
+                        camera.position.set(
+                            DESTINATION_CAMERA_POSITION.x,
+                            DESTINATION_CAMERA_POSITION.y,
+                            DESTINATION_CAMERA_POSITION.z,
+                        );
+                        camera.lookAt(
+                            DESTINATION_CAMERA_LOOK_AT.x,
+                            DESTINATION_CAMERA_LOOK_AT.y,
+                            DESTINATION_CAMERA_LOOK_AT.z,
+                        );
+                    } else if (ui_state === 'Finished') {
+                        camera.position.set(
+                            FINISHED_CAMERA_POSITION.x,
+                            FINISHED_CAMERA_POSITION.y,
+                            FINISHED_CAMERA_POSITION.z,
+                        );
+                        camera.lookAt(
+                            FINISHED_CAMERA_LOOK_AT.x,
+                            FINISHED_CAMERA_LOOK_AT.y,
+                            FINISHED_CAMERA_LOOK_AT.z,
+                        );
+                    }
                 }}
                 camera={INITIAL_CAMERA_PROPS}>
-                    <color attach={'background'} args={['#535353']} />
-                    <Perf position="top-left"/>
-                    <BoardControls/>
-                    <GameMap/>
-                </Canvas>
-                <Interface/>
-            </>
-        );
-    }
-);
+                <color attach={'background'} args={['#535353']} />
+                <Perf position="top-left" />
+                <BoardControls />
+                <GameMap />
+            </Canvas>
+            <Interface />
+        </>
+    );
+});
