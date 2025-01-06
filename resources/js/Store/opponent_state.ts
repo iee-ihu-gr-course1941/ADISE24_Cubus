41cd5560221eabb532e65e05831ca9a9fc2c6391
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-06 18:09:03 +0200

    adjust opponent state types

--- resources/js/Store/opponent_state.ts
+++ resources/js/Store/opponent_state.ts
@@ -1,18 +1,19 @@
-import {  PlayerIdentifier, Vector2 } from "@/types/piece"
+import { PlayerColor } from "@/types/game";
+import {   Vector2 } from "@/types/piece"
 import { create } from "zustand"
 
 type State = {
-    occupiedPositions: {[key in PlayerIdentifier]: Vector2[]};
+    occupiedPositions: {[key in PlayerColor]: Vector2[]};
 }
 
 type Actions = {
-    addOpponentOccupation: (positions: Vector2[], player: PlayerIdentifier) => void;
+    addOpponentOccupation: (positions: Vector2[], player: PlayerColor) => void;
 }
 
 type OpponentState = State & Actions
 export const useOpponentState = create<OpponentState>()((set, get, state) => ({
     occupiedPositions: {'green': [], 'red': [], 'blue': [], 'yellow': []},
-    addOpponentOccupation: (positions: Vector2[], player: PlayerIdentifier) => {
+    addOpponentOccupation: (positions: Vector2[], player: PlayerColor) => {
         const occupiedPositions = get().occupiedPositions;
         set({occupiedPositions: {...occupiedPositions, [player]: [...occupiedPositions[player], ...positions]}})
     }
