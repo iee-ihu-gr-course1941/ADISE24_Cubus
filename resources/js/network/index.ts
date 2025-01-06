73f1e6dc39cd958b497bca1b77603f875eb19c6b
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-06 18:07:51 +0200

    adjust network helper

--- resources/js/network/index.ts
+++ resources/js/network/index.ts
@@ -5,112 +5,121 @@ type GetRequestProps<T, K> = {
     params?: K;
     method?: 'POST' | 'PUT' | 'DELETE';
     useMinimumResponseTime?: boolean;
-  };
+};
 
-  type PostRequestProps<T, K> = GetRequestProps<T, K> & {
+type PostRequestProps<T, K> = GetRequestProps<T, K> & {
     body?: KeyType;
-  };
+};
 
-  type KeyType = {[key: string]: string | number | boolean | null | undefined};
+type KeyType = {[key: string]: string | number | boolean | null | undefined};
 
-  class Network {
+class Network {
     private static _minumumResponseTime = 500;
     private static _headers = () => {
-      return {
-        'Accept': 'application/json',
-      };
+        return {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        };
     };
 
     private static createUrl<K extends KeyType = KeyType>(params?: K): string {
-      if (params) {
-        return `?${Object.keys(params)
-          .map(key => `${key}=${params[key]}`)
-          .join('&')}`;
-      } else {
-        return '';
-      }
+        if (params) {
+            return `?${Object.keys(params)
+                .map(key => `${key}=${params[key]}`)
+                .join('&')}`;
+        } else {
+            return '';
+        }
     }
 
     static async get<T, K extends KeyType = KeyType>({
-      url,
-      formatData,
-      params,
-      useMinimumResponseTime,
+        url,
+        formatData,
+        params,
+        useMinimumResponseTime,
     }: GetRequestProps<T, K>): Promise<T | null> {
-      const urlToCall = `${url}${this.createUrl<K>(params)}`;
-      try {
-        const currentMS1 = new Date().getTime();
-        const response = await fetch(urlToCall, {
-          method: 'GET',
-          headers: {
-            ...this._headers(),
-          },
-        });
-        if (useMinimumResponseTime) {
-          const currentMS2 = new Date().getTime();
-          const timeTaken = currentMS2 - currentMS1;
-          if (timeTaken < Network._minumumResponseTime) {
-            await new Promise(resolve =>
-              setTimeout(resolve, Network._minumumResponseTime - timeTaken),
-            );
-          }
+        const urlToCall = `${url}${this.createUrl<K>(params)}`;
+        try {
+            const currentMS1 = new Date().getTime();
+            const response = await fetch(urlToCall, {
+                method: 'GET',
+                headers: {
+                    ...this._headers(),
+                },
+            });
+            if (useMinimumResponseTime) {
+                const currentMS2 = new Date().getTime();
+                const timeTaken = currentMS2 - currentMS1;
+                if (timeTaken < Network._minumumResponseTime) {
+                    await new Promise(resolve =>
+                        setTimeout(
+                            resolve,
+                            Network._minumumResponseTime - timeTaken,
+                        ),
+                    );
+                }
+            }
+            if (!response.ok || !response) {
+                return null;
+            }
+            const data = await response.json();
+            if (formatData) {
+                return formatData(data);
+            }
+            return data as T;
+            //@typescript-eslint/no-unused-vars
+        } catch (err) {
+            return null;
+        } finally {
         }
-        if (!response.ok || !response) {
-          return null;
-        }
-        const data = await response.json();
-        if (formatData) {
-          return formatData(data);
-        }
-        return data as T;
-        //@typescript-eslint/no-unused-vars
-      } catch (err) {
-        return null;
-      } finally {
-      }
     }
 
     static async post<T, K extends KeyType = KeyType>({
-      body,
-      url,
-      formatData,
-      params,
-      method = 'POST',
-      useMinimumResponseTime,
+        body,
+        url,
+        formatData,
+        params,
+        method = 'POST',
+        useMinimumResponseTime,
     }: PostRequestProps<T, K>): Promise<T | null> {
-      const urlToCall = `${url}${this.createUrl<K>(params)}`;
-      try {
-        const currentMS1 = new Date().getTime();
-        const response = await fetch(urlToCall, {
-          method: method,
-          body: JSON.stringify(body),
-          headers: {
-            ...this._headers(),
-          },
-        });
-        if (useMinimumResponseTime) {
-          const currentMS2 = new Date().getTime();
-          const timeTaken = currentMS2 - currentMS1;
-          if (timeTaken < Network._minumumResponseTime) {
-            await new Promise(resolve =>
-              setTimeout(resolve, Network._minumumResponseTime - timeTaken),
-            );
-          }
+        const urlToCall = `${url}${this.createUrl<K>(params)}`;
+        try {
+            const resToken = await fetch('http://localhost:8000/test/csrf');
+            const currentMS1 = new Date().getTime();
+            const response = await fetch(urlToCall, {
+                method: method,
+                body: JSON.stringify(body),
+                headers: {
+                    ...this._headers(),
+                    'X-CSRF-TOKEN': `${await resToken.text()}`,
+                },
+            });
+            if (useMinimumResponseTime) {
+                const currentMS2 = new Date().getTime();
+                const timeTaken = currentMS2 - currentMS1;
+                if (timeTaken < Network._minumumResponseTime) {
+                    await new Promise(resolve =>
+                        setTimeout(
+                            resolve,
+                            Network._minumumResponseTime - timeTaken,
+                        ),
+                    );
+                }
+            }
+            if (!response.ok || !response) {
+                return null;
+            }
+            const data = await response.json();
+            if (formatData) {
+                return formatData(data);
+            }
+            return data as T;
+        } catch (err) {
+            //@typescript-eslint/no-unused-vars
+            return null;
+        } finally {
         }
-        if (!response.ok || !response) {
-          return null;
-        }
-        const data = await response.json();
-        if (formatData) {
-          return formatData(data);
-        }
-        return data as T;
-      } catch (err) {
-        //@typescript-eslint/no-unused-vars
-        return null;
-      } finally {
-      }
     }
-  }
+}
 
-  export default Network;
+export default Network;
--- resources/js/network/session_network.ts
+++ resources/js/network/session_network.ts
@@ -1,10 +1,17 @@
-import Network from "."
-import { GameResponse } from "@/types/game"
-
+import Network from '.';
+import {GameResponse, MovePayload, MoveResponse} from '@/types/game';
 
 export const getGame = async () => {
     const res = await Network.get<GameResponse>({
         url: route('game.index'),
-    })
+    });
     return res;
-}
+};
+
+export const validateMove = async (move: MovePayload) => {
+    const res = await Network.post<MoveResponse>({
+        url: route('game.move'),
+        body: move,
+    });
+    return res;
+};
