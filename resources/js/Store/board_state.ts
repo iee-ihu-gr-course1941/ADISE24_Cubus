1e2d436f1ca857c1025b18edd387cba1710de26f
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-06 18:09:22 +0200

    adjust board state based on websocket connectivity

--- resources/js/Store/board_state.ts
+++ resources/js/Store/board_state.ts
@@ -1,124 +1,176 @@
-import { create } from "zustand"
-import {subscribeWithSelector} from 'zustand/middleware'
-import * as THREE from "three";
-import { GameState, MovePayload, PlayerIdentifier } from "@/types/piece";
-import { useTimerStore } from "./timer";
-import { CAMERA_ANIMATION_DURATION } from "@/Constants/camera";
+import {create} from 'zustand';
+import * as THREE from 'three';
+import {useTimerStore} from './timer';
+import {CAMERA_ANIMATION_DURATION} from '@/Constants/camera';
+import {GameState, MovePayload, PlayerColor, PlayerState} from '@/types/game';
+import {GameSession} from '@/types/models/tables/Session';
 
 type State = {
     boardRef: THREE.Mesh | null;
     boardPieces: THREE.Group[];
     move: MovePayload | null;
     gameState: GameState;
-}
+    playerState: PlayerState | null;
+};
 
 type Actions = {
-    setBoardRef: (boardRef: THREE.Mesh | null) => void
+    setState: (game: GameState, player: PlayerState) => void;
+    updateGameState: (game: GameSession) => void;
+    setBoardRef: (boardRef: THREE.Mesh | null) => void;
     addBoardPiece: (piece: THREE.Group) => void;
     setMove: (move: MovePayload | null) => void;
     rejectMove: () => void;
-    startGame: (player_count: number, player_identifier: PlayerIdentifier, player_turn: PlayerIdentifier) => void;
+    startGame: () => void;
     lockTurn: () => void;
     beginTurn: () => void;
     endTurn: () => void;
     continueTurn: () => void;
     canPlay: () => boolean;
     removeBoardPiece: (piece: THREE.Group) => void;
-    changeTurn: (player_turn?: PlayerIdentifier) => void;
-    getNextPlayer: () => PlayerIdentifier;
-}
+    changeTurn: () => void;
+};
 
-export type BoardState = State & Actions
+export type BoardState = State & Actions;
 
-export const useBoardState = create<BoardState>()((set, get, state) => ({
+export const useBoardState = create<BoardState>()((set, get, _) => ({
     boardRef: null,
     boardPieces: [],
     move: null,
     gameState: {
+        id: null,
         startTime: 0,
         endTime: 0,
-        round: 0,
-        player_turn: 'red',
-        state: 'Ready',
-        player_count: 0,
-        player_identifier: null,
+        current_round: 0,
+        ui_state: 'Ready',
+        session_state: 'waiting',
+        current_playing: 'blue',
     },
-
+    playerState: null,
+    updateGameState: game => {
+        set(prev => {
+            return {
+                gameState: {
+                    ...prev.gameState,
+                    ...game,
+                    ui_state: getUiState(game, prev.playerState),
+                },
+            };
+        });
+    },
+    setState: (game, player) => {
+        set(prev => {
+            return {
+                gameState: {
+                    ...prev.gameState,
+                    ...game,
+                    ui_state: getUiState(game, player),
+                },
+                playerState: {...prev.playerState, ...player},
+            };
+        });
+    },
     canPlay: () => {
-        return get().gameState.state === 'OwnTurnPlaying'
+        return get().gameState.ui_state === 'OwnTurnPlaying';
     },
-    startGame: (player_count, player_identifier, player_turn) => {
+    startGame: () => {
         const timer = useTimerStore.getState();
         timer.start(100);
-        set({gameState: {...get().gameState, state: 'Starting'}})
+        set({gameState: {...get().gameState, ui_state: 'Starting'}});
         setTimeout(() => {
+            const {gameState, playerState} = get();
             timer.stop();
-            set({gameState: {
-                round: 1,
-                player_identifier: player_identifier,
-                player_turn: player_turn,
-                state: player_identifier === player_turn ? 'OwnTurnPlaying' : 'OpponentTurn',
-                startTime: Date.now(),
-                player_count: player_count,
-            }})
-        }, CAMERA_ANIMATION_DURATION + 250)
+            console.log('getting player state:', get());
+            if (playerState) {
+                set(prev => ({
+                    gameState: {
+                        ...prev.gameState,
+                        ui_state:
+                            playerState.session_color ===
+                            gameState.current_playing
+                                ? 'OwnTurnPlaying'
+                                : 'OpponentTurn',
+                        startTime: Date.now(),
+                    },
+                }));
+            }
+        }, CAMERA_ANIMATION_DURATION + 250);
     },
     lockTurn: () => {
-        set({gameState: {...get().gameState, state: 'OwnTurnLocked'}})
+        set({gameState: {...get().gameState, ui_state: 'OwnTurnLocked'}});
     },
     beginTurn: () => {
         set(({gameState}) => {
             return {
                 gameState: {
-                ...gameState,round: gameState.round + 1, state: 'OwnTurnPlaying',startTime: Date.now()
-            }}
+                    ...gameState,
+                    state: 'OwnTurnPlaying',
+                    startTime: Date.now(),
+                },
+            };
         });
     },
-    changeTurn: (player_turn?: PlayerIdentifier) => {
-        set(({gameState, getNextPlayer}) => {
-            const playerTurn = player_turn ?? getNextPlayer();
+    changeTurn: () => {
+        set(({gameState}) => {
             return {
                 gameState: {
                     ...gameState,
-                    player_turn: playerTurn,
-                    state: gameState.player_identifier === (playerTurn) ? 'OwnTurnPlaying' : 'OpponentTurn',
                     startTime: Date.now(),
-                }
-            }
-        })
+                },
+            };
+        });
     },
     continueTurn: () => {
-        set(({gameState}) => ({gameState: {...gameState, state: 'OwnTurnPlaying'}}))
+        set(({gameState}) => ({
+            gameState: {...gameState, ui_state: 'OwnTurnPlaying'},
+        }));
     },
     endTurn: () => {
-        set(({gameState, getNextPlayer}) => ({gameState: {
-            ...gameState, state: 'OpponentTurn', startTime: Date.now(),
-            player_turn: getNextPlayer(),
-        }, move: null}))
+        set(({gameState}) => ({
+            gameState: {
+                ...gameState,
+                state: 'OpponentTurn',
+                startTime: Date.now(),
+            },
+            move: null,
+        }));
     },
-    setMove: (move) => {
-        set({move})
-    },
-    getNextPlayer: (): PlayerIdentifier => {
-        const currentTurn = get().gameState.player_turn;
-        const playerCount = get().gameState.player_count;
-        if(currentTurn === 'green') return 'red';
-        if(currentTurn === 'red' && playerCount===4) return 'blue';
-        if(currentTurn === 'red' && playerCount===4) return 'blue';
-        if(currentTurn === 'blue' && playerCount===4) return 'yellow';
-        return 'green';
+    setMove: move => {
+        set({move});
     },
     rejectMove: () => {
-        set(({move}) => ({move: null}))
+        set(({move}) => ({move: null}));
     },
-    setBoardRef: (boardRef) => {
-        set({boardRef})
+    setBoardRef: boardRef => {
+        set({boardRef});
     },
-    addBoardPiece: (piece) => set((prev) => {
-        if(prev.boardPieces.find(p => p.uuid === piece.uuid)) return prev
-        return {boardPieces: [...prev.boardPieces,piece]}
-    }),
-    removeBoardPiece: (piece) => set((prev) => {
-        return {boardPieces: prev.boardPieces.filter(p => p.uuid !== piece.uuid)}
-    }),
+    addBoardPiece: piece =>
+        set(prev => {
+            if (prev.boardPieces.find(p => p.uuid === piece.uuid)) return prev;
+            return {boardPieces: [...prev.boardPieces, piece]};
+        }),
+    removeBoardPiece: piece =>
+        set(prev => {
+            return {
+                boardPieces: prev.boardPieces.filter(
+                    p => p.uuid !== piece.uuid,
+                ),
+            };
+        }),
 }));
+
+const getUiState = (
+    game: GameSession,
+    player: PlayerState | undefined | null,
+) => {
+    const ui_state =
+        game.session_state === 'waiting'
+            ? 'Ready'
+            : game.session_state === 'complete'
+              ? 'Finished'
+              : game.session_state === 'playing'
+                ? game.current_playing === player?.session_color
+                    ? 'OwnTurnPlaying'
+                    : 'OpponentTurn'
+                : 'Finished';
+    return ui_state;
+};
