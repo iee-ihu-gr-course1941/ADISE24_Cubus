fa8672deb1cc8c401f6cd31134dadd1a9ed46760
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-06 18:09:10 +0200

    add moves helper libs

---
+++ resources/js/libs/move.ts
@@ -0,0 +1,37 @@
+import {MovePayload, OpponentMove} from '@/types/game';
+
+export const formatMoveOrigin = (
+    move: MovePayload,
+    playerCount?: string,
+): MovePayload => {
+    if (playerCount === '2') {
+        return {
+            ...move,
+            origin_x: Math.round((move.origin_x + 3) * 2),
+            origin_y: Math.round((move.origin_y + 3) * 2),
+        };
+    } else {
+        return {
+            ...move,
+            origin_x: Math.round((move.origin_x + 4.5) * 2),
+            origin_y: Math.round((move.origin_y + 4.5) * 2),
+        };
+    }
+};
+
+export const formatOpponentMoveOrigin = (
+    move: OpponentMove['move'],
+    playerCount?: string,
+): OpponentMove['move'] => {
+    return {
+        ...move,
+        block_positions: [
+            ...move.block_positions.map(p => ({
+                x: p.x - move.origin_x,
+                y: p.y - move.origin_y,
+            })),
+        ],
+        origin_x: move.origin_x / 2 - 3,
+        origin_y: move.origin_y / 2 - 3,
+    };
+};
