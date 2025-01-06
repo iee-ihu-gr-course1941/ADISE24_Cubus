2992ec1c136ff406944a4d6a5f7413d34130f4bf
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-06 18:10:02 +0200

    adjust game components based on websocket connectivity

--- resources/js/Game/Board/Hand.tsx
+++ resources/js/Game/Board/Hand.tsx
@@ -1,37 +1,51 @@
-import * as THREE from "three";
-import { Piece } from "./Piece/Piece";
-import { PieceCode } from "@/types/piece";
-import { useGameDimensions } from "@/Store/game_dimensions";
-import { memo } from "react";
+import * as THREE from 'three';
+import {Piece} from './Piece/Piece';
+import {PieceCode} from '@/types/piece';
+import {useGameDimensions} from '@/Store/game_dimensions';
+import {memo} from 'react';
+import {useBoardState} from '@/Store/board_state';
 
 const Hand = memo(() => {
     const blockSize = useGameDimensions(state => state.blockSize);
-    const handPositions = [new THREE.Vector3(-10, 0, -2.5), new THREE.Vector3(-3.5, 0, 1.5), new THREE.Vector3(-2.5, 0, -3.5)];
+    const validPieces = useBoardState(s => s.playerState?.session_valid_pieces);
+    const handPositions = [
+        new THREE.Vector3(-10, 0, -2.5),
+        new THREE.Vector3(-3.5, 0, 1.5),
+        new THREE.Vector3(-2.5, 0, -3.5),
+    ];
     return (
         <>
-            {
-                new Array(21).fill(0).map((_, index) => {
-                    const positionIndex = Math.floor((index/24)*3);
-                    const space = 2.5;
-                    let zPosition = 0;
-                    let xPosition = 0;
+            {new Array(21).fill(0).map((_, index) => {
+                if (!validPieces || !validPieces[index]) {
+                    return null;
+                }
+                const positionIndex = Math.floor((index / 24) * 3);
+                const space = 2.5;
+                let zPosition = 0;
+                let xPosition = 0;
 
-                    xPosition = (index % 4) * space;
-                    zPosition = Math.floor(index / 4) * space;
+                xPosition = (index % 4) * space;
+                zPosition = Math.floor(index / 4) * space;
 
-                    const position = new THREE.Vector3(positionIndex === 1 ? xPosition : zPosition, 0, positionIndex === 1 ? zPosition : xPosition).add(handPositions[positionIndex]);
-                    const rotation = Math.floor(Math.random() * 4);
-                    const flip = Math.random() > 0.5;
+                const position = new THREE.Vector3(
+                    positionIndex === 1 ? xPosition : zPosition,
+                    0,
+                    positionIndex === 1 ? zPosition : xPosition,
+                ).add(handPositions[positionIndex]);
 
-                    return (
-                      index < 21 && <Piece key={index} code={index as PieceCode} origin_position={{x: position.x, y: position.z}}
-                      rotation={0} flip={false}/>
-                    )
-
-                })
-            }
+                return (
+                    <Piece
+                        key={index}
+                        code={index as PieceCode}
+                        origin_x={position.x}
+                        origin_y={position.z}
+                        rotation={0}
+                        flip={false}
+                    />
+                );
+            })}
         </>
-    )
-})
+    );
+});
 
-export default Hand
+export default Hand;
--- resources/js/Pages/Game.tsx
+++ resources/js/Pages/Game.tsx
@@ -1,23 +1,41 @@
 import useUserEvents from '@/Connection/useUserEvents';
-import { Experience } from "@/Game/Experience";
-import { getGame } from '@/network/session_network';
-import { PageProps } from '@/types';
-import { GameResponse } from '@/types/game';
-import { User } from '@/types/models/tables/User';
-import { useEffect } from 'react';
+import {Experience} from '@/Game/Experience';
+import {useBoardState} from '@/Store/board_state';
+import {useUserEventsStore} from '@/Store/user_events_store';
+import {PageProps} from '@/types';
+import {GameResponse} from '@/types/game';
+import {User} from '@/types/models/tables/User';
+import {useEffect, useMemo} from 'react';
 
-type GameProps = PageProps<{ user: User, userSession?: GameResponse }>;
+type GameProps = PageProps<{user: User; userSession?: GameResponse}>;
 
-export default function Game({ user, userSession }: GameProps) {
-    let { connectionState, currentSession, joinGame } = useUserEvents();
-    console.log(userSession)
+export default function Game({user, userSession}: GameProps) {
+    const {currentSession, joinGame, latestMove} = useUserEvents();
+    const setState = useBoardState(state => state.setState);
+    const updateGameState = useBoardState(state => state.updateGameState);
+    const setLatestMove = useUserEventsStore(s => s.setLatestMove);
+
+    useEffect(() => {
+        setLatestMove(latestMove);
+        if (latestMove) {
+            updateGameState(latestMove.session);
+        }
+    }, [latestMove]);
+
+    const session = useMemo(() => {
+        const session = currentSession ?? userSession;
+        if (session) {
+            setState({...session.session}, {...session.player});
+        }
+
+        return session;
+    }, [currentSession, userSession]);
+
     useEffect(() => {
         /**
          * * Join Game
          */
         joinGame();
-    }, [])
-    return (
-        <Experience/>
-    );
+    }, []);
+    return <Experience />;
 }
