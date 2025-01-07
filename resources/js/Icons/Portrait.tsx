f32dbd898e63c669a8bdec062a94895f990f3a09
Dale Bezolli <dalebezolli@gmail.com>
2025-01-07 15:08:21 +0200

    [feat-design] Finalize portrait design

--- resources/js/Icons/Portrait.tsx
+++ resources/js/Icons/Portrait.tsx
@@ -5,12 +5,14 @@ type PortraitProps = {
 export function Portrait({url}: PortraitProps) {
     return (
         <div className="
-            rounded-[25px] w-fit p-2
+            rounded-[25px] w-fit p-2.5
 
             border-t border-b-2 border-t-custom-gray-700 border-b-custom-gray-800
             bg-light-default-bottom
             ">
-            <img src={url} className="w-[128px] h-[128px] rounded-[20px]" />
+            <div className="p-[1px] bg-portrait-default rounded-[21px]">
+                <img src={url} className="w-[128px] h-[128px] rounded-[20px]" />
+            </div>
         </div>
     );
 }
