cb3d41022aaf213cc71fdf0e9f733628aa4b70f5
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-08 22:08:57 +0200

    [feat-environment-design] update camera positions

--- resources/js/Constants/camera.ts
+++ resources/js/Constants/camera.ts
@@ -1,15 +1,11 @@
 import * as THREE from 'three';
 
-export const ORIGIN_CAMERA_LOOK_AT = new THREE.Vector3(0, 7.5, 0);
-export const DESTINATION_CAMERA_LOOK_AT = new THREE.Vector3(0, 0, 2.5);
+export const ORIGIN_CAMERA_LOOK_AT = new THREE.Vector3(0, 0, 0);
+export const DESTINATION_CAMERA_LOOK_AT = new THREE.Vector3(0, 7.5, 7.5);
 export const FINISHED_CAMERA_LOOK_AT = new THREE.Vector3(0, 0, 0);
 
-export const DESTINATION_CAMERA_POSITION = new THREE.Vector3(0, 9.5, 8.3);
-export const ORIGIN_CAMERA_POSITION = new THREE.Vector3(
-    0,
-    DESTINATION_CAMERA_POSITION.y * 1.25,
-    -DESTINATION_CAMERA_POSITION.z * 2,
-);
+export const ORIGIN_CAMERA_POSITION = new THREE.Vector3(-12.5, 20, 25);
+export const DESTINATION_CAMERA_POSITION = new THREE.Vector3(0, 12.5, 15);
 export const FINISHED_CAMERA_POSITION = new THREE.Vector3(0, 10, 0);
 
 export const CAMERA_ANIMATION_DURATION = 2500;
