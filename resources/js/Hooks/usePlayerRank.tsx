08eeea187c1303b895f526784e9c4f61d644c1f9
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-10 21:09:58 +0200

    add player rank hook

---
+++ resources/js/Hooks/usePlayerRank.tsx
@@ -0,0 +1,29 @@
+import {COLORS} from '@/Constants/colors';
+import {useAppState} from '@/Store/app_state';
+import {useBoardState} from '@/Store/board_state';
+import {PlayerColor} from '@/types/game';
+import {useMemo} from 'react';
+
+export const usePlayerRank = () => {
+    const session = useAppState(s => s.currentSession);
+    const playerState = useBoardState(s => s.playerState);
+
+    const position = useMemo(() => {
+        const playerColor = playerState?.session_color;
+        const playerPoints = playerColor
+            ? (session?.[`player_${playerColor}_points`] ?? 0)
+            : 0;
+        let playerPosition = 1;
+        Object.keys(COLORS).forEach(color => {
+            if (color !== playerColor) {
+                const opponentPoints =
+                    session?.[`player_${color as PlayerColor}_points`] ?? 0;
+                if (opponentPoints > playerPoints) {
+                    playerPosition = playerPosition + 1;
+                }
+            }
+        });
+        return playerPosition;
+    }, [session, playerState]);
+    return {rank: position, isWin: position === 1};
+};
