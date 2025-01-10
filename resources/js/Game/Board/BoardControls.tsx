3f6abec4e34256f45393403d621cd6d0a4395126
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-10 22:19:53 +0200

    update board controls

--- resources/js/Game/Board/BoardControls.tsx
+++ resources/js/Game/Board/BoardControls.tsx
@@ -1,4 +1,3 @@
-import {DESTINATION_CAMERA_LOOK_AT} from '@/Constants/camera';
 import {useBoardState} from '@/Store/board_state';
 import {OrbitControls} from '@react-three/drei';
 import * as THREE from 'three';
@@ -6,17 +5,19 @@ import * as THREE from 'three';
 export const BoardControls = () => {
     const ui_state = useBoardState(state => state.gameState.ui_state);
     const isGameOnGoing = useBoardState(state => state.isGameOnGoing);
+
     return (
         <>
             <OrbitControls
                 enabled={isGameOnGoing()}
+                enablePan={false}
                 target={new THREE.Vector3(0, 0, 0)}
-                minPolarAngle={Math.PI * 0.025}
-                maxPolarAngle={Math.PI * 0.35}
-                maxAzimuthAngle={Math.PI * 0.05}
-                minAzimuthAngle={Math.PI * -0.05}
-                minDistance={4}
-                maxDistance={16}
+                minPolarAngle={Math.PI * 0.035}
+                maxPolarAngle={Math.PI * 0.36}
+                maxAzimuthAngle={Math.PI * 0.1}
+                minAzimuthAngle={Math.PI * -0.1}
+                minDistance={6}
+                maxDistance={24}
                 enableZoom={true}
                 makeDefault
                 enableDamping
