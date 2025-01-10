d2e2a48354d9ffd3c97b5d138f96315c47261f36
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-10 22:12:14 +0200

    add refresh icon

--- resources/js/Icons/SVG.tsx
+++ resources/js/Icons/SVG.tsx
@@ -23,6 +23,7 @@ export enum Icon {
     rotateRight,
     mirror,
     see,
+    refresh,
 }
 
 type SVGProps = {
@@ -37,6 +38,23 @@ export function SVG({icon, fill, stroke, className, size = 16}: SVGProps) {
     let svg: React.ReactElement;
 
     switch (icon) {
+        case Icon.refresh:
+            svg = (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6">
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                </svg>
+            );
+            break;
         case Icon.see:
             svg = (
                 <svg
